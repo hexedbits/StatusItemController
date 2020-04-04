ccb22dc93d6ea0cc4b31fafbb9b4b40114de351c
Jesse Squires <jesse@jessesquires.com>
2020-04-04 14:43:07 -0700

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Documentation
+//  https://hexedbits.github.io/StatusItemController
+//
+//  GitHub
+//  https://github.com/hexedbits/StatusItemController
+//
+//  Copyright © 2020-present Jesse Squires, Hexed Bits
+//  https://www.hexedbits.com
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "StatusItemController",
+    platforms: [
+        .macOS(.v10_12)
+    ],
+    products: [
+        .library(
+            name: "StatusItemController",
+            targets: ["StatusItemController"])
+    ],
+    targets: [
+        .target(
+            name: "StatusItemController",
+            path: "Sources"),
+        .testTarget(name: "StatusItemControllerTests",
+                    dependencies: ["StatusItemController"],
+                    path: "Tests")
+    ],
+    swiftLanguageVersions: [.v5]
+)
