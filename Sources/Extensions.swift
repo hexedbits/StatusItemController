784de653107098d643c709978a6f5416b6c6d169
Jesse Squires <jesse@jessesquires.com>
2020-04-04 17:06:07 -0700

    write docs for extensions

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -15,6 +15,9 @@
 import Cocoa
 
 extension NSEvent {
+
+    /// Returns `true` if the event is `.rightMouseUp` or equivalent.
+    /// Returns `false` otherwise.
     public var isRightClickUp: Bool {
         let rightClick = (self.type == .rightMouseUp)
         let controlClick = self.modifierFlags.contains(.control)
@@ -23,6 +26,9 @@ extension NSEvent {
 }
 
 extension NSApplication {
+
+    /// Returns `true` if the application's current event is `.rightMouseUp` or equivalent.
+    /// Returns `false` otherwise.
     public var isCurrentEventRightClickUp: Bool {
         if let current = self.currentEvent {
             return current.isRightClickUp
@@ -32,6 +38,14 @@ extension NSApplication {
 }
 
 extension NSMenuItem {
+
+    /// A convenience init for `NSMenuItem`.
+    /// - Parameters:
+    ///   - title: The title of the menu item.
+    ///   - target: The target to be associated with the menu item.
+    ///   - action: The action selector to be associated with the menu item.
+    ///   - keyEquivalent: A string representing a keyboard key to be used as the key equivalent.
+    ///   - isEnabled: A Boolean value that indicates whether the menu item is enabled.
     public convenience init(title: String,
                             target: AnyObject,
                             action: Selector? = nil,
