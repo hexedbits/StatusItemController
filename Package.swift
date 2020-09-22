1840045cf11bc7a54331e0f67dc5b5cce0fbc7d4
Jesse Squires <jesse@jessesquires.com>
2020-09-21 19:09:26 -0700

    update package.swift to 5.3 tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  Created by Jesse Squires
@@ -29,10 +29,12 @@ let package = Package(
     targets: [
         .target(
             name: "StatusItemController",
-            path: "Sources"),
+            path: "Sources",
+            exclude: ["Info.plist"]),
         .testTarget(name: "StatusItemControllerTests",
                     dependencies: ["StatusItemController"],
-                    path: "Tests")
+                    path: "Tests",
+                    exclude: ["Info.plist"]),
     ],
     swiftLanguageVersions: [.v5]
 )
