f49c080775cbbbe7160fdafd6e2db35f46ef126b
Jesse Squires <jesse@jessesquires.com>
2022-02-04 15:52:07 -0800

    Infra updates
    - update GH workflows
    - update swiftlint
    - update danger
    - update bundle

--- scripts/lint.zsh
+++ scripts/lint.zsh
@@ -12,7 +12,7 @@
 PROJECT="StatusItemController.xcodeproj"
 SCHEME="StatusItemController"
 
-VERSION="0.45.0"
+VERSION="0.46.2"
 
 FOUND=$(swiftlint version)
 LINK="https://github.com/realm/SwiftLint"
